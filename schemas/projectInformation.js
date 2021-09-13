6f9535c4e8d6d7476d6dbe4bd00b7d14e669f9be
jsimpson15011 <joseph.franklin.simpson@gmail.com>
2021-09-12 19:17:28 -0600

    Initial Commit

---
+++ schemas/projectInformation.js
@@ -0,0 +1,51 @@
+export default {
+  name: "project",
+  type: "document",
+  title: "Projects",
+  fields:[
+    {
+      name: "title",
+      type: "string",
+      title: "Project Title"
+    },
+    {
+      name: "subTitle",
+      type: "string",
+      title: "Project Subtitle"
+    },
+    {
+      name: "description",
+      type: "text",
+      title: "Description"
+    },
+    {
+      name: "image",
+      type: "mainImage",
+      title: "Main Project Image",
+    },
+    {
+      name: "backgroundImage",
+      type: "image",
+      title: "Background Image"
+    },
+    {
+      name: 'tags',
+      type: 'array',
+      title: 'Tags',
+      description: 'Keywords used for filtering projects',
+      of: [{
+        type: 'reference',
+        to: [{type: 'projectTag'}]
+      }]
+    },
+    {
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {
+        source: "title",
+        maxLength: 96
+      }
+    }
+  ]
+}
---
+++ schemas/projectTag.js
@@ -0,0 +1,17 @@
+export default {
+  name: "projectTag",
+  title: "Project Tag",
+  type: "document",
+  fields: [
+    {
+      name: "title",
+      type: "string",
+      title: "Tag Title"
+    },
+    {
+      name: "icon",
+      type: "image",
+      title: "Tag Icon"
+    }
+  ]
+}
